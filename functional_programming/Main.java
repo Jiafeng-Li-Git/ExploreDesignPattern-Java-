619fd6d7289ab158a818ac45923f1b99901a9bfc
Jiafeng-Li-Git <jli740@uottawa.ca>
2023-01-05 17:04:52 -0500

    explore streams and lambdas

--- functional_programming/Main.java
+++ functional_programming/Main.java
@@ -1,5 +1,10 @@
 package functional_programming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -21,8 +26,27 @@ public class Main {
 		GreetingInterface greeting3 = () -> System.out.println("Hello Lambda");
 		greeting3.sayHello();
 		
-		//lambda with arguments
+		// lambda with arguments
 		SumInterface summing = (int a, int b) -> {return a + b;};
 		System.out.println(summing.sum(1, 2));
+		
+		// explore stream
+		Integer[] intArray = new Integer[] {1, 2, 5, 9};
+		Stream<Integer> intStream = Arrays.stream(intArray);
+		
+		List<String> shops = new ArrayList<String>();
+		shops.add("Loblaws");
+		shops.add("Farmboy");
+		Stream<String> shopStream = shops.stream();
+		System.out.println(shops);
+		shopStream.sorted()
+		.filter(shop -> shop.charAt(0)=='L')
+		.forEach(shop -> System.out.println(shop));
+		System.out.println(shops);
+		
+		Stream<String> letterStream = Stream.of("hello", " stream");
+		
+		
+		
 	}
 }
